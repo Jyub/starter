7666906a580fd6901a54af5e021a74a21977c2b5
yubiao <literyu@foxmail.com>
2020-01-08 18:03:37 +0800

    1

---
+++ m_actuator/src/main/java/com/m/m_actuator/MActuatorApplication.java
@@ -0,0 +1,13 @@
+package com.m.m_actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MActuatorApplication.class, args);
+	}
+
+}
